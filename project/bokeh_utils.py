8154d3ca04396dfb95f2ba75be80a0cc83612e66
MaximilianBurger <m.burger@clara-compliance.com>
2023-09-16 22:33:53 +0200

    Functioning Line color animation

---
+++ project/bokeh_utils.py
@@ -0,0 +1,60 @@
+from networkx import multipartite_layout
+
+from bokeh import events
+from bokeh.io import output_file, output_notebook, curdoc
+from bokeh.plotting import figure, show, from_networkx
+from bokeh.models import ColumnDataSource, Circle, MultiLine
+from bokeh.layouts import row, column, gridplot
+from bokeh.models import ColumnDataSource, CustomJS, Slider
+from bokeh.colors import Color
+
+LINE_COLOR = 'line_color'
+LINE_COLOR_LIST = 'line_color_list'
+
+LINE_ALPHA = 'line_alpha'
+LAYER='layer'
+
+def draw_interactive_mlp_graph(G):
+    graph = from_networkx(G, layout_function=multipartite_layout(G, subset_key=LAYER))
+
+
+    graph.node_renderer.glyph = Circle(size=15, fill_color="lightblue")
+
+    # to initialize the Line color with something
+    line_color_list = graph.edge_renderer.data_source.data[LINE_COLOR_LIST]
+    graph.edge_renderer.data_source.data[LINE_COLOR] =  [l[0] for l in line_color_list]
+    graph.edge_renderer.glyph = MultiLine(
+        line_color=LINE_COLOR, # the field of the edges
+        line_width=0.3
+    )
+    plot = figure()
+
+    callback = CustomJS(
+        args=dict(source=graph.edge_renderer.data_source), 
+        code="""
+            // make a shallow copy of the current data dict
+            const new_data = Object.assign({}, source.data)
+
+            // update the y column in the new data dict from the appropriate other column
+            const X = source.data['line_color_list']
+
+            new_data.line_color  = X.map(subArray => subArray[cb_obj.value]);
+
+            // set the new data on source, BokehJS will pick this up automatically
+            source.data = new_data
+        """)
+      
+    slider = Slider(
+        start=0, 
+        end=len(graph.edge_renderer.data_source.data[LINE_COLOR_LIST][0]) - 1, 
+        value=0, 
+        step=1, 
+        title="iteration"
+    )
+    slider.update
+    
+    slider.js_on_change('value', callback)
+
+    plot.renderers.append(graph)
+    layout = column(slider, plot)
+    show(layout)
--- project/nx_utils.py
+++ project/nx_utils.py
@@ -8,19 +8,35 @@ STATE_DICT = "model_state_dict"
 POSITIVE_COLOR = "blue"
 NEGATIVE_COLOR = "red"
 
+def black_and_white(layers_of_weights):
 
-def positive_negative(G, edge, weight_pos, matrix_idx):
-    #color = POSITIVE_COLOR if weight > 0 else NEGATIVE_COLOR
-    #return "color", color
-    pass
+    layers = []
+    for weight_matrix in layers_of_weights:
+        X = weight_matrix.numpy()
+        mask = X > 0
+
+        # Create X with the same shape as Y and fill it with 'no'
+        Y = np.full(X.shape, fill_value='red', dtype=object)
+
+        # Fill X with 'yes' where Y is True
+        Y[mask] = 'blue'
+
+        layers.append(Y)
+    
+    return layers
 
 def alpha_value(weights):
+    # TODO: remove complexity of this function style.
+    # simply use a list of matrices.
+
     norm_weights = [ np.abs(w)/ np.abs(w).max() for w in weights]
 
     def func(G, edge, weight_pos, matrix_idx):
-        return "alpha", norm_weights[matrix_idx][weight_pos].item()
+        W = norm_weights[matrix_idx]
+        return W[weight_pos].item()
 
     return func
+
 
 def load_state_dict_from_file(file: Path):
     return load(file)[STATE_DICT]
@@ -29,55 +45,71 @@ def get_shape_from_state_dict(state_dict):
     shapes = [v.shape for k,v in state_dict.items() if 'weight' in k]
     return shapes
 
-def torch_state_dict_unpacker(state_dict):
+def get_weights_and_biases_from_state_dict(state_dict):
     weights = [v for k,v in state_dict.items() if 'weight' in k]
     biases = [v for k,v in state_dict.items() if 'bias' in k]
     return weights, biases
 
-def add_weight_edges(
+def get_layers_of_nodes(G: nx.DiGraph):
+    """
+    Returns a dictionary of Attributes as keys, 
+    with a list of nodes that have that attribute as the value
+    """
+    mydict = nx.get_node_attributes(G, 'layer')
+    layers_of_nodes = []
+    for key, value in mydict.items():
+        if value == len(layers_of_nodes):
+            layers_of_nodes.append([])
+
+        layers_of_nodes[value].append(key)
+
+    return layers_of_nodes
+
+
+
+# Populate the Graph
+
+def add_weight_edges_arrays(
     G: nx.DiGraph,
-    list_of_weights, 
-    layers_to_nodes_dict, 
-    iteration,
-    attribute_functions,  # list of functions that return attribute and value
+    layers_to_nodes, 
+    attribute_set,  # list of functions that return attribute and value
 ):
+    """Inplace add attributes to all edges, according to attribute functions."""
     
-    attrs = {}
-
-    for matrix_idx, _ in enumerate(list_of_weights):
-
-        in_layer = layers_to_nodes_dict[matrix_idx]
-        out_layer = layers_to_nodes_dict[matrix_idx+1]
-
-        for i, in_node in enumerate(in_layer):
-            for o, out_node in enumerate(out_layer):
-                weight_pos = (o,i)
-                edge = (in_node, out_node)
-
-                attrs[edge] = {}
-                for func in attribute_functions:
-                    name, attribute = func(G, edge, weight_pos, matrix_idx)
-                    attrs[edge][f'{name}-{iteration}'] = attribute
+    attrs = {} 
+    for name, layers_of_weights in attribute_set.items():
+        for layer, timeful_weight_matrix in enumerate(layers_of_weights):
+            for i, in_node in enumerate(layers_to_nodes[layer]):
+                for o, out_node in enumerate(layers_to_nodes[layer+1]):
+                    graph_pos = (in_node, out_node)
+                    attrs[graph_pos] = {name : timeful_weight_matrix[:, o, i]}
 
     nx.set_edge_attributes(G, attrs)
 
-def add_weight_edges_deprecated(G: nx.DiGraph, list_of_weights, layers, idx):
-    # go through all weight matrices of the network
-    edge_attribute_values = {}
 
-    for i, W in enumerate(list_of_weights):
+def add_weight_edges(
+    G: nx.DiGraph,
+    layers_to_nodes, 
+    iteration,
+    attribute_functions,  # list of functions that return attribute and value
+):
+    """Inplace add attributes to all edges, according to attribute functions."""
+    
+    attrs = {}
 
-        in_layer = layers[i]
-        out_layer = layers[i+1]
+    for layer in range(len(layers_to_nodes)-1):
+        for i, in_node in enumerate(layers_to_nodes[layer]):
+            for o, out_node in enumerate(layers_to_nodes[layer+1]):
 
-        OUT,IN = W.shape
-        for i in range(IN):
-            for o in range(OUT):
-                w = W[o][i]
-                edge = (in_layer[i], out_layer[o])
-                edge_attribute_values[edge] = {f'weight_{idx}':w.item()}
+                weight_pos = (o, i)
+                edge = (in_node, out_node)
 
-    nx.set_edge_attributes(G, edge_attribute_values)
+                attrs[edge] = {}
+
+                for name, func in attribute_functions.items():
+                    attrs[edge][f'{name}-{iteration}'] = func(G, edge, weight_pos, layer) * 0.1 * iteration
+
+    nx.set_edge_attributes(G, attrs)
 
 def add_neuron_nodes(G: nx.DiGraph, weight_shapes):
     """Create all nodes of a neural network from the weight matrices."""
@@ -99,50 +131,3 @@ def add_neuron_nodes(G: nx.DiGraph, weight_shapes):
         for a in layers[-2]:
             for b in layers[-1]:
                 G.add_edge(a, b)
-
-def get_dict_of_node_layers(G: nx.DiGraph):
-    """
-    Returns a dictionary of Attributes as keys, 
-    with a list of nodes that have that attribute as the value
-    """
-    mydict = nx.get_node_attributes(G, 'layer')
-    reversed_dict = {}
-    for key, value in mydict.items():
-        reversed_dict.setdefault(value, [])
-        reversed_dict[value].append(key)
-
-    return reversed_dict
-
-def add_neuron_nodes_deprecated(G: nx.DiGraph, list_of_weights):
-    """Create all nodes of a neural network from the weight matrices."""
-    node_id = count(0)
-
-    layers = []
-
-    # go through all weight matrices of the network
-    for W in list_of_weights:
-
-        OUT,IN = W.shape
-
-        if not layers:
-            layer = len(layers)
-            layers.append([])
-            for _ in  range(IN):
-                node = next(node_id)
-                G.add_node(node, layer=layer)
-                layers[-1].append(node)
-
-
-        layer = len(layers)
-        layers.append([])
-        for _ in  range(OUT):
-            node = next(node_id)
-            G.add_node(node, layer=layer)
-            layers[-1].append(node)
-
-        # create edges between all nodes of neighbouring layers
-        for a in layers[-2]:
-            for b in layers[-1]:
-                G.add_edge(a, b)
-
-    return layers
