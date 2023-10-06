64680452b9ab40b453ce8f04fbb813dc42a63231
mxmn <max_burger@gmx.at>
2023-10-06 13:25:18 +0200

    remove logging from loop and add device

--- project/common/training.py
+++ project/common/training.py
@@ -1,29 +1,35 @@
 import wandb
+import torch
 import numpy as np
 from torch import optim
 from common.torch_utils import measure_global_sparsity
+from common.tracking import Config
 
 EVAL_LOSS = 'loss/eval'
 TRAIN_LOSS = 'loss/train'
 EPOCH = 'epoch'
 
-def evaluate(model, loader, loss_fn):
+def evaluate(model, loader, loss_fn, device):
     """Evaluate the model and return a numpy array of losses for each batch."""
+
     model.eval()
     losses = []
-    for _, (x, y) in enumerate(loader):
-        pred  = model(x)
-        loss = loss_fn(pred, y)
-        losses.append(loss.detach().cpu().numpy())
+    with torch.no_grad():
+        for _, (x, y) in enumerate(loader):
+            x,y = x.to(device), y.to(device)
+
+            pred  = model(x)
+            loss = loss_fn(pred, y)
+            losses.append(loss.detach().cpu().numpy())
 
     return np.array(losses)
 
-def update(model, loader, optim, loss_fn):
+def update(model, loader, optim, loss_fn, device):
     """Update the model and return a numpy array of losses for each batch."""
-
     model.train()
     losses = []
     for _, (x, y) in enumerate(loader):
+        x,y = x.to(device), y.to(device)
         optim.zero_grad()
         pred  = model(x)
         loss =  loss_fn(pred, y)
@@ -33,22 +39,20 @@ def update(model, loader, optim, loss_fn):
 
     return np.array(losses)
 
-def train_and_evaluate(model, train_loader, test_loader, optim, loss_fn, epochs=1):
+def train_and_evaluate(model, train_loader, test_loader, optim, loss_fn, device, epochs=1):
     """Train a model for the specified amount of epochs."""
-        
-    N0, N, sparsity = measure_global_sparsity(model, use_mask=True)
-    wandb.log({'nonzero' : N-N0, 'sparsity': sparsity}, commit=False)
-    
+
+    train_losses, eval_losses = [], []
+
     # train for epochs
-    for epoch in range(1, epochs+1):
-        loss_train = update(model, train_loader, optim, loss_fn).mean().item()
-        loss_eval = evaluate(model, test_loader, loss_fn).mean().item()
-        
-        wandb.log({
-            TRAIN_LOSS + f"/{sparsity:.2f}" : loss_train,
-            EVAL_LOSS + f"/{sparsity:.2f}": loss_eval,
-            EPOCH : epoch
-        })
+    for _ in range(0, epochs):
+        loss_train = update(model, train_loader, optim, loss_fn, device).mean()
+        loss_eval = evaluate(model, test_loader, loss_fn, device).mean().item()
+
+        train_losses += [loss_train]
+        eval_losses += [loss_eval]
+
+    return train_losses, eval_losses
 
 def train(model, loader, optim, loss_fn, epochs=1):
     """
@@ -62,7 +66,7 @@ def train(model, loader, optim, loss_fn, epochs=1):
 
     return np.array(losses)
 
-def build_optimizer(model, config):
+def build_optimizer(model, config: Config):
     """inspired by wandb
     https://colab.research.google.com/github/wandb/examples/blob/master/colabs/pytorch/Organizing_Hyperparameter_Sweeps_in_PyTorch_with_W%26B.ipynb#scrollTo=KfkduI6qWBrb
     """
@@ -79,3 +83,13 @@ def build_optimizer(model, config):
         )
 
     return optimizer
+
+def build_model(config: Config):
+    ModelClass = config.model_class
+    model = ModelClass.make_from_config(config)
+    loss_fn = torch.nn.MSELoss(reduction="mean")
+
+    # send to device
+    model.to(config.device)
+
+    return model, loss_fn
