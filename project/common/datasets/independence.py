2fd30cc1e68f6a6b5cf7eec6d7a7dddabdd2cdda
mxmn <max_burger@gmx.at>
2023-10-13 13:09:25 +0200

    refactor dataset

--- project/common/datasets/independence.py
+++ project/common/datasets/independence.py
@@ -15,7 +15,15 @@ DATASET_NAME = "Independence_Symbolic"
 _N = 100
 _seed = 0
 
-def _f(data):
+
+
+def f1(x1, x3):
+    return (x1+x3)**3
+
+def f2(x2, x4):
+    return x2**2+np.sin(np.pi*x4)
+
+def f(data):
     """
     The function that transforms input to output.
     from BIMT code
@@ -24,19 +32,23 @@ def _f(data):
     x2 = data[:,[1]]
     x3 = data[:,[2]]
     x4 = data[:,[3]]
-    out = np.transpose(np.array([(x1+x3)**3, x2**2+np.sin(np.pi*x4)]))
+
+    y1 = f1(x1,x3)
+    y2 = f2(x2, x4)
+
+    out = np.array([y1, y2]).T
     return out.squeeze()
 
 def get_train_and_test_data():
     """from BIMT code"""
     torch_utils.set_seed(_seed)
     inputs = np.random.rand(_N, INPUT_DIM)*2-1
-    labels = _f(inputs)
+    labels = f(inputs)
     inputs = torch.tensor(inputs, dtype=torch.float, requires_grad=True)
     labels = torch.tensor(labels, dtype=torch.float, requires_grad=True)
 
     inputs_test = np.random.rand(_N, INPUT_DIM)*2-1
-    labels_test = _f(inputs_test)
+    labels_test = f(inputs_test)
     inputs_test = torch.tensor(inputs_test, dtype=torch.float, requires_grad=True)
     labels_test = torch.tensor(labels_test, dtype=torch.float, requires_grad=True)
 
