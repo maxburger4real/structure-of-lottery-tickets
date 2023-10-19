79de98aad0c11fd07981c16f732dd52a47465e72
mxmn <max_burger@gmx.at>
2023-10-19 20:46:02 +0200

    merge plot running into one file

--- project/run_plots.py
+++ project/run_plots.py
@@ -1,15 +1,20 @@
 from common import tracking
-from run_plot import plot
+from common import plotting
 
+def plot(path):
+    plotting.plot_checkpoints(path)
+ 
 def main():
-    first = '2023_10_09_205114'
-    last = '2023_10_09_214342'
-    model_class = 'common.architectures.SimpleMLP_4_20_20_2'
-    path = tracking.persistance_path / model_class 
-    selection = [dir for dir in path.iterdir() if dir.stem >= first and dir.stem <= last]
 
-    for path in selection:
-        plot(path)
+    models = {
+        'SimpleMLP_4_20_20_2' : ["ssoy8hp4", ],
+        'BioMLP_4_20_20_2' : ["qv2wt6or"]
+    }
+
+    for model_class, runs in models.items(): 
+        for name in runs:
+            path = tracking.persistance_path / model_class / name
+            plot(path)
 
 if __name__ == '__main__':
     main()
