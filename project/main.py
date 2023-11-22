9c47841db112483908dc6f43ec52a2492a02ba3b
mxmn <max_burger@gmx.at>
2023-11-22 11:27:07 +0100

    merge plot into main cmd line tool

--- project/main.py
+++ project/main.py
@@ -10,8 +10,7 @@ import wandb
 import argparse
 import argcomplete
 
-from settings import WANDB_DIR
-import plot_nn
+from settings import WANDB_DIR, RUNS_DATA_DIR
 
 from common.training_pipelines import pipeline_selector
 from common.models import build_model_from_config
@@ -21,6 +20,7 @@ from common.config import Config, import_config
 from common.pruning_trajectory import update_pruning_config
 from common.training import build_loss
 from common.constants import *
+from common.plotting import plot_checkpoints
 
 
 def run_experiment(config, mode=None):
@@ -55,43 +55,73 @@ def run_experiment(config, mode=None):
 
         return str(run.id)
 
+def plot(run_ids=None, sweep_ids=None):
+    """plot any number of runs or sweeps."""
+    if run_ids is not None:
+        for name in run_ids:
+            plot_checkpoints(RUNS_DATA_DIR / name)
+
+    if sweep_ids is not None:
+        for name in sweep_ids:
+            api = wandb.Api()
+            sweep = api.sweep(f"{ENTITY}/{PROJECT}/{name}")
+            run_ids = [run.id for run in sweep.runs]
+
+            for name in run_ids:
+                plot_checkpoints(RUNS_DATA_DIR / name)
+
 def main():
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('-r', '--run_config', help='run config file', required=True)
-    parser.add_argument('-s', '--sweep_config', help='sweep config file')
-    parser.add_argument('-d', '--disable', action='store_true', help="Disable WANDB mode.")
-    parser.add_argument('-p', '--plot', action='store_true', help="plot nn.")
-    parser.add_argument('--count', help="The maximum number of runs for this sweep")
     argcomplete.autocomplete(parser)  # Enable autocompletion with argcomplete
-    args = parser.parse_args()  # Parse the arguments
+
+    # runs
+    parser.add_argument('-r', '--run_config', help='run config file', required=True)
+    parser.add_argument('-p', '--plot', action='store_true', help="plot nn that are created. No sweeps yet.")
+
+    # sweeps
+    parser.add_argument('-s', '--sweep_config', help='sweep config file')
+    parser.add_argument('--count', help="The maximum number of runs for this sweep")
+
+    # for plotting seperatedly
+    parser.add_argument('--run_ids', nargs='+', help="The run ids to plot. Any number ")
+    parser.add_argument('--sweep_ids',  nargs='+',  help="The sweep ids to plot. Any number ")
+    
+    # debug - disbale wandb logging to speedup
+    parser.add_argument('-d', '--disable', action='store_true', help="Disable WANDB mode.")
+    
+    # THE ARGHSSSSSS
+    args = parser.parse_args()
+
+    # read count if it exists or ignore
+    count = None
+    if args.count is not None:
+        if args.count.isdigit():
+            count = int(args.count)
+        else: 
+            print(f'Ignoring args.count {args.count}')
 
     run_config = import_config(args.run_config)
     sweep_config = import_config(args.sweep_config)
     mode = 'disabled' if args.disable else None
     
-    count = None
-    if args.count is not None:
-        if args.count.isdigit():
-            count = int(args.count)
-
+    # skip
     if run_config is None:
-        raise ValueError('No run config provided')
-
-    if sweep_config is not None:
-        # initialize the sweep
-        sweep_id = wandb.sweep(sweep_config, ENTITY, PROJECT)
-
-        # start execution of the sweeps
-        function = lambda : run_experiment(run_config, mode)
-        wandb.agent(sweep_id, function, ENTITY, PROJECT, count)
-
-
-    else:
+        print('No run config provided')
+    
+    # single run
+    elif sweep_config is None:
         id = run_experiment(run_config, mode)
+        if args.plot: plot(run_ids=[id])
 
-        if args.plot:
-            plot_nn.main(run_ids=[id])
+    # sweep
+    else: 
+       
+        sweep_id = wandb.sweep(sweep_config, ENTITY, PROJECT)  # initialize the sweep
+        function = lambda : run_experiment(run_config, mode)
+        wandb.agent(sweep_id, function, ENTITY, PROJECT, count) # start execution of the sweeps
+
+    plot(run_ids=args.run_ids, sweep_ids=args.sweep_ids )
 
 
 if __name__ == "__main__":
