c1b18cc8ce1f935f6994cb8d8e3a0eb3a650cbeb
mxmn <max_burger@gmx.at>
2023-12-26 12:04:09 +0100

    update logging

--- project/common/training_pipelines/imp.py
+++ project/common/training_pipelines/imp.py
@@ -1,9 +1,9 @@
 from tqdm import tqdm
-from common.log import Logger, returns_true_every_nth_time
+from common.log import Logger
 from common.config import Config
 from common.nxutils import GraphManager
 from common.persistance import save_model_or_skip
-from common.pruning import build_pruning_func, build_reinit_func, count_prunable_params
+from common.pruning import build_pruning_func, build_reinit_func
 from common.training import build_early_stopper, build_optimizer, update, evaluate
 from common.constants import *
 
@@ -12,28 +12,24 @@ def run(model, train_loader, test_loader, loss_fn, config: Config):
     # preparing for pruning and [OPTIONALLY] save model state
     prune = build_pruning_func(model, config)
     reinit = build_reinit_func(model)
-    gm = GraphManager(model, config.model_shape, config.task_description, config.extension_levels) if config.log_graphs else None
-    log = Logger(config.task_description, True)
+    gm = GraphManager(model, config.model_shape, config.task_description) if config.log_graphs else None
+    log = Logger(gm, config.task_description, config.log_graphs_before_split)
     save_model_or_skip(model, config, f'{-config.extension_levels}_init')
     
     # log initial performance and descriptive statistics
     init_loss, init_accuracy = evaluate(model, test_loader, loss_fn, config.device)
     log.metrics({VAL_LOSS:init_loss, ACCURACY:init_accuracy, 'level': -config.extension_levels-1})
-    # log.descriptive_statistics(model, at_init=True)
     log.commit()  # LOG BEFORE TRAINING
 
     # get the complete levels
     levels = list(range(-config.extension_levels, config.pruning_levels))
-
-    pparams = config.param_trajectory[0]
-    pborder = 0
+    pparams, pborder = config.param_trajectory[0], 0
 
     ####################
     ### Training 
     ####################
     for level, pruning_amount in tqdm(zip(levels, config.pruning_trajectory, strict=True), 'Pruning Levels', len(levels)):
 
-        # train and evaluate the model and log the performance
         optim = build_optimizer(model, config)
         stopper = build_early_stopper(config)
 
@@ -47,7 +43,6 @@ def run(model, train_loader, test_loader, loss_fn, config: Config):
                     values={TRAIN_LOSS : train_loss.mean(), VAL_LOSS : val_loss, ACCURACY : val_acc.mean()},
                 )
                 log.commit()
-                # log.descriptive_statistics(model, prefix=f'epochwise-descriptive')
 
             if config.loss_cutoff is not None and val_loss.mean().item() < config.loss_cutoff: break
             if stopper(val_loss.mean().item()): break
@@ -55,19 +50,20 @@ def run(model, train_loader, test_loader, loss_fn, config: Config):
         save_model_or_skip(model, config, level)
 
         # log weights, biases, metrics at early stopping iteration
-        log.metrics({TRAIN_LOSS : train_loss, VAL_LOSS : val_loss, ACCURACY : val_acc})
-        log.metrics({'pparams' : pparams, 'level' : level, 'stop' : epoch, 'pborder' : pborder})
-        # log.descriptive_statistics(model)
-
-        gm.update(model) if gm is not None else None
-        log.feature_categorization(gm)
-        log.splitting(gm)
-        log.graphs(gm)
+        log.metrics({
+            TRAIN_LOSS : train_loss, VAL_LOSS : val_loss, ACCURACY : val_acc,
+            'pparams' : pparams, 'level' : level, 'stop' : epoch, 'pborder' : pborder
+        })
         
-        # there is only one commit in each Pruning LEVEL! except in-epoch logging is active
-        log.commit()  # LOG PRUNING LEVEL
+        if gm is not None:
+            gm.update(model, level) 
+            log.feature_categorization()
+            log.splitting()
+            if gm.iteration == gm.split_iteration: log.graphs()
+            log.commit()
 
-        if gm.untapped_potential < 0 and config.stop_on_degradation: break
+            if gm.untapped_potential < 0 and config.stop_on_degradation: 
+                break
 
         # prune the model and reinit
         pborder = prune(pruning_amount)
@@ -95,13 +91,13 @@ def run(model, train_loader, test_loader, loss_fn, config: Config):
             log.commit()
 
     log.metrics({TRAIN_LOSS : train_loss, VAL_LOSS : val_loss, ACCURACY : val_acc})
-    # log.descriptive_statistics(model)
 
-    gm.update(model) if gm is not None else None
-    log.feature_categorization(gm)
-    log.splitting(gm)
-    log.graphs(gm)
-    log.summary(gm)
-    log.commit()
+    if gm is not None:
+        gm.update(model, config.pruning_levels)
+        log.feature_categorization()
+        log.splitting()
+        if gm.iteration == gm.split_iteration: log.graphs()
+        log.summary()
+        log.commit()
 
     save_model_or_skip(model, config, config.pruning_levels)
