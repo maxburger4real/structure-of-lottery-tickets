89d3b427f7d13908ee59180f42c582af35b71e93
mxmn <max_burger@gmx.at>
2023-12-27 17:28:36 +0100

    new configs and sweeps

--- project/sweepconfigs/initializations.py
+++ project/sweepconfigs/initializations.py
@@ -3,31 +3,19 @@
 from common.constants import InitializationStrategy
 seeds = list(range(10,15))
 seeds = list(range(10))
+
 seeds = list(range(100,110))
-seeds = [42,17,444]
 
-w = (
-        #InitializationStrategy.KAIMING_NORMAL.name,
-        InitializationStrategy.XAVIER_NORMAL.name,
-        #InitializationStrategy.XAVIER_UNIFORM.name,
-    )
-
-shapes = (
-    (2,25,1),
-    (2,40,1),
-    (2,60,1),
-    (2,10,10,1),
-    (2,15,15,1),
-    (2,20,20,1),
+init_strategies = (
+    InitializationStrategy.KAIMING_NORMAL.name,
+    InitializationStrategy.DEFAULT.name,
 )
 
 sweep_config = {
     'method': 'grid', 
-    'name': f'Testing with multiple seeds : {seeds}',
+    'name': f'Test initialization for weights with new early stopping.',
     'parameters':{
-        "init_strategy_weights":  {"values": w},
+        "init_strategy_weights":  {"values": init_strategies},
         "model_seed":  { "values": seeds },
-        "activation": { "values" : ['sigmoid']},
-        "model_shape": { "values" : shapes}
         }
     }
