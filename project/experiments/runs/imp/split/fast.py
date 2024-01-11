53b344cf0441beb453d9ffa492a88e0a83e0b0b1
mxmn <max_burger@gmx.at>
2024-01-11 11:36:52 +0100

    cleanup dataset and runconfigs

--- project/experiments/runs/imp/split/fast.py
+++ project/experiments/runs/imp/split/fast.py
@@ -2,14 +2,21 @@ from common.models import MLP
 from common.config import Config
 from common.constants import *
 
+
 run_config = Config(
-    description='''Good splitting. Testing the new early stopping.''',
+
+    pruning_levels=6, # 6, 7, 8
+    model_seed=SEEDS_123[0],  # splits with any of the seeds
+
     pipeline=IMP,
     activation=RELU,
     loss_fn=BCE,    
-    dataset=Datasets.CIRCLES_AND_MOONS.name,
 
-    model_shape=[4, 320, 320, 2],
+    dataset=Datasets.CIRCLES_AND_MOONS.name,
+    n_samples=1000,
+    noise=0.1,
+
+    model_shape=[4, 410, 410, 2],
     model_class=MLP.__name__,
     scaler=StandardUnitVariance,
 
@@ -19,13 +26,8 @@ run_config = Config(
     epochs= 3000,
     batch_size=64,
     
-    # seeds
-    model_seed=8,
     data_seed=0,
-
     persist=False,
-
-    # early stop
     early_stop_patience=30,
     early_stop_delta=0.0,
 
@@ -34,13 +36,9 @@ run_config = Config(
     pruning_scope=GLOBAL,
     prune_biases=False,
     prune_weights=True,
-    pruning_target=50,
-    pruning_levels=20,
-    reinit=True,
 
-    # newly added 
-    init_strategy_weights = InitializationStrategy.DEFAULT.name,
+    pruning_target=112,  # 4*8 + 8*8 + 8*2  --> [4,8,8,2]
+    reinit=True,
+    init_strategy_weights = InitializationStrategy.KAIMING_NORMAL.name,
     init_strategy_biases = InitializationStrategy.ZERO.name,
-    n_samples=1000,
-    noise=0.1
 )
--- project/experiments/runs/vanilla/circles-moons.py
+++ project/experiments/runs/vanilla/circles-moons.py
@@ -3,46 +3,34 @@ from common.config import Config
 from common.constants import *
 
 run_config = Config(
-    description='''Good performance perfect split.''',
-    pipeline=IMP,
+    description='''Early stopping works well.''',
+    pipeline=VANILLA,
     activation=RELU,
-    loss_fn= BCE,    
-    dataset=Datasets.CIRCLES_AND_MOONS.name,  # works reliably
-    
+    loss_fn=BCE,    
+    dataset=Datasets.CIRCLES_AND_MOONS.name,
+
     model_shape=[4, 320, 320, 2],
-    model_class = MLP.__name__,
+    model_class=MLP.__name__,
     scaler=StandardUnitVariance,
 
     # training
     lr=0.001,
     optimizer=ADAM,
     epochs= 3000,
+    batch_size=64,
     
     # seeds
-    model_seed=7,
+    model_seed=8,
     data_seed=0,
-
     persist=False,
 
     # early stop
     early_stop_patience=30,
     early_stop_delta=0.0,
 
-    # yielded good results
-    loss_cutoff=0.01,
-
-    # pruning
-    pruning_method=MAGNITUDE,
-    pruning_scope=GLOBAL,
-    prune_biases=True,
-    prune_weights=True,
-    pruning_target=50,
-    pruning_levels=20,
-    reinit=True,
-
     # newly added 
     init_strategy_weights = InitializationStrategy.DEFAULT.name,
-    init_strategy_biases = InitializationStrategy.DEFAULT.name,
+    init_strategy_biases = InitializationStrategy.ZERO.name,
     n_samples=1000,
     noise=0.1
 )
